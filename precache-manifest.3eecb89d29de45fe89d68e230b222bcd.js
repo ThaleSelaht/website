7508dcdec389507e520ab3866b76ce1732bdf86d
ThaleSelaht <thalespizza@gmail.com>
2018-12-07 11:16:54 -0200

    Updates

---
+++ precache-manifest.3eecb89d29de45fe89d68e230b222bcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "87d22494cd186bc3447bc890e3fbef9a",
+    "url": "/website/static/media/funil-de-contas.87d22494.png"
+  },
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "f6930d72034bbf25ec5d",
+    "url": "/website/static/js/main.f6930d72.chunk.js"
+  },
+  {
+    "revision": "6a12ee261120fad06742",
+    "url": "/website/static/js/1.6a12ee26.chunk.js"
+  },
+  {
+    "revision": "f6930d72034bbf25ec5d",
+    "url": "/website/static/css/main.563b1b36.chunk.css"
+  },
+  {
+    "revision": "6a12ee261120fad06742",
+    "url": "/website/static/css/1.c8df85a3.chunk.css"
+  },
+  {
+    "revision": "7fc88c32ef7f2f39c2debd4cc06db549",
+    "url": "/website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.821c903491dd4cd00d1b54b89298b122.js"
+  "/website/precache-manifest.3eecb89d29de45fe89d68e230b222bcd.js"
 );
 
 workbox.clientsClaim();
