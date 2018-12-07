5f4e5b6837478011595de8045343ffd249ed5d8e
ThaleSelaht <thalespizza@gmail.com>
2018-12-07 10:27:14 -0200

    Updates

---
+++ precache-manifest.0f0ad76774ae060a758a758df4b2f25e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "87d22494cd186bc3447bc890e3fbef9a",
+    "url": "/dashboard/static/media/funil-de-contas.87d22494.png"
+  },
+  {
+    "revision": "81312d4a81e499095a3d",
+    "url": "/dashboard/static/js/runtime~main.81312d4a.js"
+  },
+  {
+    "revision": "db3f18da8e1ef30af696",
+    "url": "/dashboard/static/js/main.db3f18da.chunk.js"
+  },
+  {
+    "revision": "743944c615cfae616f58",
+    "url": "/dashboard/static/js/1.743944c6.chunk.js"
+  },
+  {
+    "revision": "db3f18da8e1ef30af696",
+    "url": "/dashboard/static/css/main.fb8dd9e3.chunk.css"
+  },
+  {
+    "revision": "743944c615cfae616f58",
+    "url": "/dashboard/static/css/1.c8df85a3.chunk.css"
+  },
+  {
+    "revision": "3a0391fda8c0657d2fc118174c2f8eaa",
+    "url": "/dashboard/index.html"
+  }
+];
