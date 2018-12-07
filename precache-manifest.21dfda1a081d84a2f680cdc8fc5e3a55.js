da500f0ea1bd99e4e8506d6d4270fa35164b48e0
ThaleSelaht <thalespizza@gmail.com>
2018-12-07 10:11:09 -0200

    Updates

---
+++ precache-manifest.21dfda1a081d84a2f680cdc8fc5e3a55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "005c05c8e3fd1bbe879a",
+    "url": "/website/static/js/main.005c05c8.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/website/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "005c05c8e3fd1bbe879a",
+    "url": "/website/static/css/main.8f9785e7.chunk.css"
+  },
+  {
+    "revision": "b3d18da4aeff67e54d2e319a675204fe",
+    "url": "/website/index.html"
+  }
+];
